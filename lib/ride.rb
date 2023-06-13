848a46a9556420a9622b16290598eb33e7f25583
Davis Weimer <davisweimer@gmail.com>
2023-06-13 09:40:46 -0600

    feat: #total_revenue

--- lib/ride.rb
+++ lib/ride.rb
@@ -2,11 +2,13 @@ class Ride
   attr_reader :name,
               :min_height,
               :admission_fee,
-              :excitement
+              :excitement,
+              :total_revenue
   def initialize(ride)
     @name = ride[:name]
     @min_height = ride[:min_height]
     @admission_fee = ride[:admission_fee]
     @excitement = ride[:excitement]
+    @total_revenue = 0
   end
 end

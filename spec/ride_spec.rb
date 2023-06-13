77917251314919d207baca00ad1eb8e974775daf
Davis Weimer <davisweimer@gmail.com>
2023-06-13 10:29:24 -0600

    add_test: #board_rider() & #rider_log

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -6,6 +6,8 @@ RSpec.describe Ride do
     @visitor2 = Visitor.new('Tucker', 36, '$5')
     @visitor3 = Visitor.new('Penny', 64, '$15')
     @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
   end
 
   describe 'class' do
@@ -39,6 +41,18 @@ RSpec.describe Ride do
       expect(@visitor1.spending_money).to eq(8)
       expect(@visitor2.spending_money).to eq(4)
       expect(@visitor3.spending_money).to eq(15)
+      
+      @visitor2.add_preference(:thrilling)
+      expect(@visitor2.preferences).to eq([:gentle, :thrilling])
+      
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+      
+      expect(@ride3.total_revenue).to eq(4)
+      expect(@visitor1.spending_money).to eq(8)
+      expect(@visitor2.spending_money).to eq(2)
+      expect(@visitor3.spending_money).to eq(13)
     end
   end
 
@@ -56,6 +70,18 @@ RSpec.describe Ride do
       expect(@ride1.rider_log).to eq({
         @visitor1=>2,
         @visitor2=>1
+      })
+
+      @visitor2.add_preference(:thrilling)
+      expect(@visitor2.preferences).to eq([:gentle, :thrilling])
+
+      @ride3.board_rider(@visitor1)
+      @ride3.board_rider(@visitor2)
+      @ride3.board_rider(@visitor3)
+
+      expect(@ride3.rider_log).to eq({
+        @visitor2=>1,
+        @visitor3=>1
       })
     end
   end

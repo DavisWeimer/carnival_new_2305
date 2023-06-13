d4540e8214ef25927891ed57d751ec71ccf90552
Davis Weimer <davisweimer@gmail.com>
2023-06-13 09:27:29 -0600

    test: #tall_enough?()

--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -3,6 +3,8 @@ require 'spec_helper'
 RSpec.describe Visitor do
   before(:each) do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
   end
 
   describe 'class' do
@@ -25,6 +27,16 @@ RSpec.describe Visitor do
       @visitor1.add_preference(:gentle)
       @visitor1.add_preference(:thrilling)
       expect(@visitor1.preferences).to eq([:gentle, :thrilling])
+    end
+  end
+
+  describe '#tall_enough?()' do
+    it 'can check if a Visitor meets or exceeds a height threshold' do
+      expect(@visitor1.tall_enough?(54)).to eq true
+      expect(@visitor2.tall_enough?(54)).to eq false
+      expect(@visitor3.tall_enough?(54)).to eq true
+      
+      expect(@visitor1.tall_enough?(64)).to eq false
     end
   end
 end

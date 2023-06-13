4082e1bf7bc8e44894a22bc81520ae095fdab1bb
Davis Weimer <davisweimer@gmail.com>
2023-06-13 09:37:50 -0600

    test_&_feat: Ride class attributes

--- spec/ride_spec.rb
+++ spec/ride_spec.rb
@@ -5,11 +5,16 @@ RSpec.describe Ride do
     @visitor1 = Visitor.new('Bruce', 54, '$10')
     @visitor2 = Visitor.new('Tucker', 36, '$5')
     @visitor3 = Visitor.new('Penny', 64, '$15')
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
   end
 
   describe 'class' do
     it 'exists' do
       expect(@ride1).to be_a(Ride)
+      expect(@ride1.name).to eq("Carousel")
+      expect(@ride1.min_height).to eq(24)
+      expect(@ride1.admission_fee).to eq(1)
+      expect(@ride1.excitement).to eq(:gentle)
     end
   end
 end

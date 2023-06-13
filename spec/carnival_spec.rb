08abae5fdefd2df055631e51e3bb4d132bb407d9
Davis Weimer <davisweimer@gmail.com>
2023-06-13 10:40:03 -0600

    test: Carnival class

---
+++ spec/carnival_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+RSpec.describe Carnival do
+  before(:each) do
+    @carnival = Carnival.new(14)
+    
+    @ride1 = Ride.new({ name: 'Carousel', min_height: 24, admission_fee: 1, excitement: :gentle })
+    @ride2 = Ride.new({ name: 'Ferris Wheel', min_height: 36, admission_fee: 5, excitement: :gentle })
+    @ride3 = Ride.new({ name: 'Roller Coaster', min_height: 54, admission_fee: 2, excitement: :thrilling })
+
+    @visitor1 = Visitor.new('Bruce', 54, '$10')
+    @visitor2 = Visitor.new('Tucker', 36, '$5')
+    @visitor3 = Visitor.new('Penny', 64, '$15')
+
+
+  end
+
+  describe 'class' do
+    it 'exists' do
+      expect(@carnival).to be_a(Carnival)
+      expect(@carnival.duration).to eq(14)
+      expect(@carnival.rides).to eq([])
+    end
+  end
+end
